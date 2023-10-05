e319ac0ab1002d36404b3fb07788a6e877d4137c
rahir2267 <146333984+rahir2267@users.noreply.github.com>
2023-10-05 13:25:35 +0100

    sweet pg 4

---
+++ sweets.py
@@ -0,0 +1,8 @@
+stud=input("enter no of sweets")
+size=input("enter number of pupils")
+stud=int(stud)
+size=int(size)
+a= stud/size
+a=int(a)
+print("sweets given to each pupil",a)
+print("no of sweets left are",stud%size)
